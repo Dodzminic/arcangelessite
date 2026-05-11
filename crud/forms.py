4ea209cbdc684d263ecc07dbef879e6102316ef8
Desriel Arcangeles <dodzmotovlog@gmail.come.com>
2026-05-11 10:49:42 +0800

    Final Version: Added Password Confirmation and Full Sidebar Logic

--- crud/forms.py
+++ crud/forms.py
@@ -1,19 +1,38 @@
 from django import forms
-from .models import UserProfile
+from .models import UserProfile, Gender
 
 class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput(), label="Password")
-    confirm_password = forms.CharField(widget=forms.PasswordInput(), label="Confirm Password") # Criteria 7 (3%)
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter password'}),
+        label="Password"
+    )
+    confirm_password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Confirm password'}),
+        label="Confirm Password"
+    )
 
     class Meta:
         model = UserProfile
-        fields = ['username', 'password', 'confirm_password', 'email', 'profile_pic', 'gender']
+        fields = ['username', 'email', 'gender', 'profile_pic']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'gender': forms.Select(attrs={'class': 'form-select'}),
+        }
 
     def clean(self):
         cleaned_data = super().clean()
         password = cleaned_data.get("password")
         confirm_password = cleaned_data.get("confirm_password")
 
-        if password != confirm_password: # Criteria 7 (3%)
+        if password and confirm_password and password != confirm_password:
             raise forms.ValidationError("Passwords do not match!")
         return cleaned_data
+
+class GenderForm(forms.ModelForm):
+    class Meta:
+        model = Gender
+        fields = ['gender']
+        widgets = {
+            'gender': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'New gender option...'}),
+        }
--- crud/views.py
+++ crud/views.py
@@ -1,43 +1,61 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.core.paginator import Paginator
-from .models import UserProfile, Gender # Make sure Gender is imported
-from .forms import UserForm
+from django.contrib import messages
+from .models import UserProfile, Gender
+from .forms import UserForm, GenderForm
 
 def user_list(request):
     query = request.GET.get('q', '')
     sort_by = request.GET.get('sort', '-id') 
-    users = UserProfile.objects.filter(username__icontains=query)
+    
+    # Sidebar Logic
+    genders = Gender.objects.all()
+    gender_form = GenderForm()
 
-    if sort_by == 'name_asc':
-        users = users.order_by('username')
-    elif sort_by == 'name_desc':
-        users = users.order_by('-username')
-    elif sort_by == 'oldest':
-        users = users.order_by('id')
-    else:
-        users = users.order_by('-id')
+    if request.method == "POST" and 'add_gender' in request.POST:
+        form = GenderForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'New gender added!')
+            return redirect('user_list')
+
+    if request.method == "POST" and 'delete_gender' in request.POST:
+        gender_id = request.POST.get('gender_id')
+        gender = get_object_or_404(Gender, id=gender_id)
+        try:
+            gender.delete()
+            messages.warning(request, 'Gender removed.')
+        except:
+            messages.error(request, 'Cannot delete: assigned to students.')
+        return redirect('user_list')
+
+    # Filtering & Sorting
+    users = UserProfile.objects.filter(username__icontains=query)
+    if sort_by == 'name_asc': users = users.order_by('username')
+    elif sort_by == 'name_desc': users = users.order_by('-username')
+    elif sort_by == 'oldest': users = users.order_by('id')
+    else: users = users.order_by('-id')
 
     paginator = Paginator(users, 5)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
+    page_obj = paginator.get_page(request.GET.get('page'))
     
     return render(request, 'user_list.html', {
-        'page_obj': page_obj, 
-        'query': query, 
-        'sort_by': sort_by,
-        'total_users': users.count() 
+        'page_obj': page_obj, 'query': query, 'sort_by': sort_by,
+        'total_users': users.count(), 'genders': genders, 'gender_form': gender_form
     })
 
-# THIS IS THE MISSING FUNCTION CAUSING YOUR ERROR
 def add_user(request):
     if request.method == "POST":
         form = UserForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            user = form.save(commit=False)
+            # user.password = form.cleaned_data['password'] # For demo purposes
+            user.save()
+            messages.success(request, 'Student added successfully!')
             return redirect('user_list')
     else:
         form = UserForm()
-    return render(request, 'form.html', {'form': form, 'title': 'Add New User'})
+    return render(request, 'form.html', {'form': form, 'title': 'Add New Student'})
 
 def edit_user(request, pk):
     user = get_object_or_404(UserProfile, pk=pk)
@@ -45,12 +63,13 @@ def edit_user(request, pk):
         form = UserForm(request.POST, request.FILES, instance=user)
         if form.is_valid():
             form.save()
+            messages.success(request, 'Profile updated!')
             return redirect('user_list')
     else:
         form = UserForm(instance=user)
-    return render(request, 'form.html', {'form': form, 'title': 'Edit User'})
+    return render(request, 'form.html', {'form': form, 'title': 'Edit Student'})
 
 def delete_user(request, pk):
-    user = get_object_or_404(UserProfile, pk=pk)
-    user.delete()
+    get_object_or_404(UserProfile, pk=pk).delete()
+    messages.error(request, 'Student deleted.')
     return redirect('user_list')
